ad4abc4199bd758db64e7a4ccf1c5a7e79c816f8
Kashish Grover <kashish.grover@thoughtspot.com>
2020-02-08 02:07:37 +0530

    React Native Vector Icons basic example

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Example1Screen from './screens/Example1Screen';
+import Example1Screen from './screens/Example1/Example1Screen';
 import Example2Screen from './screens/Example2Screen';
 import Example3Screen from './screens/Example3Screen';
 import Example4Screen from './screens/Example4Screen';

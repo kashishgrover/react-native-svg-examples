5db60060e14ad335e7624327ab68bea95c8c3efa
Kashish Grover <kashish.grover@thoughtspot.com>
2020-02-08 11:22:45 +0530

    Example 3

--- src/screens/Example3/Example3Screen.js
+++ src/screens/Example3/Example3Screen.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
-// import FilterExample from './filter-example.svg';
-// import Homer from './homer-simpson.svg';
-// import Tiger from './tiger.svg';
+import FileIcon from './040-file-picture.svg';
+import FilterExample from './filter-example.svg';
+import Homer from './homer-simpson.svg';
+import Tiger from './tiger.svg';
 
 const Example3Screen = () => {
   return (
     <ScrollView>
-      {/* <FilterExample /> */}
-      {/* <Homer height={300} width={300} /> */}
-      {/* <Tiger height={300} width={300} /> */}
+      <FileIcon fill="#ff0000" />
+      <FilterExample />
+      <Homer height={300} width={300} />
+      <Tiger height={300} width={300} />
     </ScrollView>
   );
 };
--- src/screens/Example4/Example4Screen.js
+++ src/screens/Example4/Example4Screen.js
@@ -1,35 +1,12 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Image } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 
 const Example4Screen = () => {
   return (
     <ScrollView>
-      <Image source={require('./040-file-picture.svg')} style={styles.icon} />
-      <Image source={require('./filter-example.svg')} />
-      <Image source={require('./homer-simpson.svg')} style={styles.homer} />
-      <Image source={require('./graphic-mesh-network.svg')} style={styles.mesh} />
-      <Image source={require('./tiger.svg')} style={styles.mesh} />
+      <Text>Switch to master branch to view this example</Text>
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  icon: {
-    tintColor: '#ff0000',
-    height: 64,
-    width: 64,
-  },
-  homer: {
-    height: 300,
-    width: 300,
-  },
-  mesh: {
-    height: 300,
-    width: 300,
-  },
-});
 
 export default Example4Screen;
